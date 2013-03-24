d2b3709af4a93323101c320353344b15ee98b44f
li0524158198 <li0524158198@163.com>
2013-03-24 10:54:13 +0800

    Initial Commit

---
+++ Simple Table/BIDViewController.h
@@ -0,0 +1,13 @@
+//
+//  BIDViewController.h
+//  Simple Table
+//
+//  Created by liweihua on 13-3-24.
+//  Copyright (c) 2013年 iy1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BIDViewController : UIViewController
+
+@end
---
+++ Simple Table/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Simple Table
+//
+//  Created by liweihua on 13-3-24.
+//  Copyright (c) 2013年 iy1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BIDAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BIDAppDelegate class]));
+    }
+}
